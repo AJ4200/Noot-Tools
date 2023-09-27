9fb7cb392b5460fdc4d09728bb0357585cf0849c
Abel Majadibodu <Abeljackson33@gmail.com>
2023-09-27 15:09:39 +0200

    fonts and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
+import Background from "@/components/Background";
+import CenterPiece from "@/components/CenterPiece";
+
 export default function Home() {
   return (
-    <>
-      <div className="waveWrapper waveAnimation">
-        <div className="waveWrapperInner bgTop">
-          <div
-            className="wave waveTop"
-            style={{
-              backgroundImage:
-                "url('http://front-end-noobs.com/jecko/img/wave-top.png')",
-            }}
-          ></div>
-        </div>
-        <div className="waveWrapperInner bgMiddle">
-          <div
-            className="wave waveMiddle"
-            style={{
-              backgroundImage:
-                "url('http://front-end-noobs.com/jecko/img/wave-mid.png')",
-            }}
-          ></div>
-        </div>
-        <div className="waveWrapperInner bgBottom">
-          <div
-            className="wave waveBottom"
-                       style={{
-              backgroundImage:
-                "url('http://front-end-noobs.com/jecko/img/wave-bot.png')",
-            }}
-          ></div>
-        </div>
-      </div>
+    <>   <CenterPiece/>
+      <Background />
+   
     </>
   );
 }
---
+++ src/components/CenterPiece.tsx
@@ -0,0 +1,22 @@
+import React from'react';
+ 
+
+ 
+const CenterPiece: React.FC = () => 
+{
+return (
+  <figure className="place-self-center self-center">
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+    <h1 className="Logo">Noot-Tools</h1>
+  </figure>
+);
+}
+export default CenterPiece;
