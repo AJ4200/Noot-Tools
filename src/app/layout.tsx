226a208277783cfe4ee743a027ec54a7cbb03934
Abel Majadibodu <Abeljackson33@gmail.com>
2023-10-12 16:22:18 +0200

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import './styles/bg.css'
 import './styles/centerpiece.css'
+import './styles/noot.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
